e0d814a3b1f3f647d0d564516a6fae13ff95b36a
supremeamerapplication <supremeamerapplication@gmail.com>
2026-01-14 22:51:44 +0100

    Update supabase.js

--- public/js/config/supabase.js
+++ public/js/config/supabase.js
@@ -1,8 +1,9 @@
-// public/js/config/supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
 const SUPABASE_URL = 'https://mkccjauzbknuebrezfyy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rY2NqYXV6YmtudWVicmV6Znl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzODI3MjMsImV4cCI6MjA4Mzk1ODcyM30.cWHqls8a2dUkBQI0el4BfALET9jDz54CD_gLzarhdmA';
 
-// Initialize Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-export { supabase };
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
