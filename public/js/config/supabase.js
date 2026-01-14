188804b02856fcb6252a2f6b267c25826b327c7f
supremeamerapplication <supremeamerapplication@gmail.com>
2026-01-14 14:41:41 +0100

    Update supabase.js

--- public/js/config/supabase.js
+++ public/js/config/supabase.js
@@ -1,6 +1,6 @@
 // public/js/config/supabase.js
-const SUPABASE_URL = 'https://pnesafdvblgdnsuxuerj.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuZXNhZmR2YmxnZG5zdXh1ZXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMzM1OTQsImV4cCI6MjA4MzkwOTU5NH0.R_Sw5gpBKRWVQ6Nw6HLRq4ELkc4t6pdC3KNgrHVypos';
+const SUPABASE_URL = 'https://mkccjauzbknuebrezfyy.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rY2NqYXV6YmtudWVicmV6Znl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzODI3MjMsImV4cCI6MjA4Mzk1ODcyM30.cWHqls8a2dUkBQI0el4BfALET9jDz54CD_gLzarhdmA';
 
 // Initialize Supabase client
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
